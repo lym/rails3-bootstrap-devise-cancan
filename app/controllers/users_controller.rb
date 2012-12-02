55248ab6f6c5776f10893e1d708cfcf10baaee8b
Salym Senyonga <salymsash@gmail.com>
2012-12-02 20:37:32 +0300

    Finished the tutorial bit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  before_filter :authenticate_user!
+
+  def index
+    authorize! :index, @user, :message => 'Not authorized as an administrator.'
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def update
+    authorize! :update, @user, :message => 'Not authorized as an administrator.'
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user], :as => :admin)
+      redirect_to users_path, :notice => "User updated."
+    else
+      redirect_to users_path, :alert => "Unable to update user."
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+puts 'Creating Roles'
+Role.create([
+  { :name => 'admin' },
+  { :name => 'user' },
+  { :name => 'VIP' }
+], :without_protection => true)
+
+puts 'Setting Up Default User Login'
+user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please'
+puts 'New User Created: ' << user.name
+user2 = User.create! :name => 'Second User', :email => 'user2@example.com', :password => 'please', :password_confirmation => 'please'
+puts 'New User Created' << user2.name
+user.add_role :admin
+user2.add_role :VIP
